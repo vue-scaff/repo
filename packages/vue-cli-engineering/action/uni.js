5eb0e24ae8525ccd63e4d6797b7970e97f5ced21
Joenix <joenix@qq.com>
2021-06-06 17:57:42 +0800

    update

--- packages/vue-cli-engineering/action/uni.js
+++ packages/vue-cli-engineering/action/uni.js
@@ -1,6 +1,6 @@
 // Check Target bind Global
 function check(name) {
-  return ![undefined, "undefined"].includes(typeof global[name]);
+  return ![undefined, 'undefined'].includes(typeof global[name]);
 }
 
 // Made Target Assign Proto
@@ -22,28 +22,28 @@ const properties = {
         docHeight: document.documentElement.clientHeight,
 
         winWidth: window.screen.width,
-        winHeight: window.screen.height
+        winHeight: window.screen.height,
       };
 
       return {
-        deviceId: "13817152426",
+        deviceId: '0',
         language: navigator.language,
-        model: "PC",
+        model: 'PC',
         pixelRatio: 2,
-        platform: "mac",
+        platform: 'mac',
         safeArea: {
           bottom: space.docHeight,
           height: space.docHeight,
           left: 0,
           right: space.docWidth,
           top: 0,
-          width: space.docWidth
+          width: space.docWidth,
         },
         safeAreaInsets: {
           top: 0,
           right: 0,
           bottom: 0,
-          left: 0
+          left: 0,
         },
         screenHeight: space.winHeight,
         screenWidth: space.winWidth,
@@ -52,27 +52,39 @@ const properties = {
         windowBottom: 0,
         windowHeight: space.docHeight,
         windowTop: 0,
-        windowWidth: space.docWidth
+        windowWidth: space.docWidth,
       };
     },
 
-    getMenuButtonBoundingClientRect() {}
+    getMenuButtonBoundingClientRect() {},
   },
 
   wx: {
     canIUse() {
       return false;
-    }
+    },
   },
 
   weex: {
-    requireModule() {}
-  }
+    requireModule() {},
+  },
 };
 
+// Tolerant of Faults
+function tolerant(setting = {}, rant = {}) {
+  // Loop
+  for (let key in setting) {
+    // Set emmm... Set
+    const set = setting[key];
+
+    if (set === true) {
+      rant[key] = make(key);
+    }
+  }
+
+  // Export
+  return rant;
+}
+
 // First Time Check Uni
-export default {
-  uni: make("uni"),
-  wx: make("wx"),
-  weex: make("weex")
-};
+export default tolerant(process.env.rc.rant || {});
