4e8113f8e1e6acb6c1d44aae4f40f68c3b92dff4
Joenix <joenix@qq.com>
2021-06-25 14:03:43 +0800

    update

--- packages/vue-cli-plugin/ext/index.js
+++ packages/vue-cli-plugin/ext/index.js
@@ -1,17 +1,19 @@
-const inf = require("./inf");
-const command = require("./command");
-const concert = require("./concert");
-const preset = require("./preset");
-const defer = require("./defer");
-const merge = require("./merge");
-const alias = require("./alias");
-const matching = require("./matching");
-const rem = require("./rem");
-const kit = require("./kit");
-const mock = require("./mock");
+const inf = require('./inf');
+const rc = require('./rc');
+const command = require('./command');
+const concert = require('./concert');
+const preset = require('./preset');
+const defer = require('./defer');
+const merge = require('./merge');
+const alias = require('./alias');
+const matching = require('./matching');
+const rem = require('./rem');
+const kit = require('./kit');
+const mock = require('./mock');
 
 module.exports = {
   inf,
+  rc,
   command,
   concert,
   preset,
@@ -21,5 +23,5 @@ module.exports = {
   matching,
   rem,
   kit,
-  mock
+  mock,
 };
